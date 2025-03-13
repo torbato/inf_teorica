e3ae323af4a76c3a6a302678680d764c75f36d8a
torbato <giovanetorbato@outlook.com>
2025-03-13 09:21:44 +0100

    add bin_to_dec converter

---
+++ numeri/bin_to_dec.py
@@ -0,0 +1,35 @@
+def get_number():
+    number_string = input("Input a binary number: ").strip()
+    return number_string
+
+def convert():
+    binary_temp = get_number()
+    temp_bin = []
+    
+    for i in range(0,len(binary_temp)):
+        temp_bin.append(binary_temp[i])
+
+    temp_dec = []
+    
+    n = len(temp_bin) - 1
+    
+    for m in range(0, len(temp_bin)):
+        if n >= 0:
+            temp_dec.append(int(temp_bin[m])*(2 ** n))
+        else:
+            return
+        n = n-1
+        
+    result = 0
+
+    for num in temp_dec:
+        result = result + num
+
+    return result
+
+def main():
+    result = convert()
+    print(f"The number converted to decimal is {result}\n")
+
+if __name__ == "__main__":
+    main()
